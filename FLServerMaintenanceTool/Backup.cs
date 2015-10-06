add70ffff8ba53497b82c8aacb3901aecaa8f771
Robert Eves <roberteves92@gmail.com>
2015-10-06 19:30:55 +0100

    added shortcut to accounts folders and added directory copy

--- FLServerMaintenanceTool/Backup.cs
+++ FLServerMaintenanceTool/Backup.cs
@@ -7,10 +7,12 @@ namespace FLServerMaintenanceTool
 {
     class Backup
     {
+        //Application Properties shortcuts
         string BackupFolder { get { return Properties.Settings.Default.BackupsFolder; } }
+        string AccountsFolder { get { return Properties.Settings.Default.AccountsFolder; } }
 
         string TimedBackupFolder { get { return BackupFolder + now.Year + "-" + now.Month + "-" + now.Day + " " + now.Hour + "-" + now.Minute + "-" + now.Second + "\\"; } }
-
+        string AccountsBackupFolder { get { return TimedBackupFolder + "\\Accounts\\"; } }
         DateTime now;
 
         public Backup()
@@ -23,6 +25,9 @@ namespace FLServerMaintenanceTool
         {
             //Create Timed backup folder
             Directory.CreateDirectory(TimedBackupFolder);
+
+            //Copy player accounts / server settings
+            DirectoryCopy(AccountsFolder, AccountsBackupFolder, true);
         }
 
         private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
