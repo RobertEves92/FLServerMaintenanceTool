d40c48f248f0f8fc46e44c1de7f7cb2783e8bc83
Robert Eves <roberteves92@gmail.com>
2015-10-06 18:24:21 +0100

    added close handlers

--- FLServerMaintenanceTool/SettingsForm.cs
+++ FLServerMaintenanceTool/SettingsForm.cs
@@ -10,9 +10,43 @@ namespace FLServerMaintenanceTool
 {
     public partial class SettingsForm : Form
     {
+        private static bool warnClose = true;
         public SettingsForm()
         {
             InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SettingsForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (warnClose)
+            {
+                DialogResult result = MessageBox.Show("Cancel all changes?", "Cancel?", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+                if (result == System.Windows.Forms.DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //Save new settings
+            Properties.Settings.Default.FLFolder = txtFreelancerFolder.Text;
+            Properties.Settings.Default.AccountsFolder = txtAccountFolder.Text;
+            Properties.Settings.Default.BackupsFolder = txtBackupFolder.Text;
+            Properties.Settings.Default.ServerAddress = txtServerAddress.Text;
+            Properties.Settings.Default.ServerPassword = txtServerPassword.Text;
+            Properties.Settings.Default.CloseProcesses = txtProcessesShutdown.Text;
+            Properties.Settings.Default.RestartProcesses = txtProcessesRestart.Text;
+            Properties.Settings.Default.Save();
+            //close without warning
+            warnClose = false;
+            this.Close();
         }
     }
 }
