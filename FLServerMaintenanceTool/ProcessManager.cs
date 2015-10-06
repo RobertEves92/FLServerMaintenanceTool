ad1911ee22d21f8a1b56b6034f73a190fb894967
Robert Eves <roberteves92@gmail.com>
2015-10-06 19:44:20 +0100

    added class with methods to close/kill and start a process

---
+++ FLServerMaintenanceTool/ProcessManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace FLServerMaintenanceTool
+{
+    class ProcessManager
+    {
+        public void CloseProcess(string processname)
+        {
+            foreach (var process in Process.GetProcessesByName(processname))
+            {
+                //Close process and free resources
+                process.CloseMainWindow();
+                process.Close();
+
+                System.Threading.Thread.Sleep(5000); //wait 5s
+
+                //if the process continues to run after 5s, kill it
+                if (!process.HasExited)
+                    process.Kill();
+            }
+        }
+
+        public void StartProcess(string processpath)
+        {
+            Process.Start(processpath);
+        }
+    }
+}
