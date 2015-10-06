7a9854b3d6e414190896e77f916b62ce11298f4f
Robert Eves <roberteves92@gmail.com>
2015-10-06 17:45:38 +0100

    Added form controls

--- FLServerMaintenanceTool/MainForm.Designer.cs
+++ FLServerMaintenanceTool/MainForm.Designer.cs
@@ -29,13 +29,57 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.pgrCountdown = new System.Windows.Forms.ProgressBar();
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // pgrCountdown
+            // 
+            this.pgrCountdown.Location = new System.Drawing.Point(12, 12);
+            this.pgrCountdown.Maximum = 60;
+            this.pgrCountdown.Name = "pgrCountdown";
+            this.pgrCountdown.Size = new System.Drawing.Size(260, 23);
+            this.pgrCountdown.TabIndex = 0;
+            this.pgrCountdown.Value = 60;
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.Location = new System.Drawing.Point(12, 38);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(173, 13);
+            this.lblCountdown.TabIndex = 1;
+            this.lblCountdown.Text = "Time to Auto Maintainance: 60mins";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(257, 167);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start Maintainance Now";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblCountdown);
+            this.Controls.Add(this.pgrCountdown);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -43,10 +87,16 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FL Server Maintenance Tool";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar pgrCountdown;
+        private System.Windows.Forms.Label lblCountdown;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ FLServerMaintenanceTool/Strings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FLServerMaintenanceTool
+{
+    public static class Strings
+    {
+        public static string CountdownTime { get { return "Time to Auto Maintainance: %1"; } }
+    }
+}
