0461db8605755a4c3de52f36820f146383997245
Robert Eves <roberteves92@gmail.com>
2015-10-06 19:25:28 +0100

    created backup class with date time initialise and shortcuts to backup folder and timed backup folder

---
+++ FLServerMaintenanceTool/Backup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FLServerMaintenanceTool
+{
+    class Backup
+    {
+        string BackupFolder { get { return Properties.Settings.Default.BackupsFolder; } }
+
+        string TimedBackupFolder { get { return BackupFolder + now.Year + "-" + now.Month + "-" + now.Day + " " + now.Hour + "-" + now.Minute + "-" + now.Second + "\\"; } }
+
+        DateTime now;
+
+        public Backup()
+        {
+            //Get Current Date and Time
+            now = DateTime.Now;
+        }
+    }
+}
