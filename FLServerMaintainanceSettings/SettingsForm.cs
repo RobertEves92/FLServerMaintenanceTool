036155b8de98bce087b0ae1a45533f89202225d4
Robert Eves <roberteves92@gmail.com>
2015-10-07 13:39:04 +0100

    updated settings tool to use Ini class library

--- FLServerMaintainanceSettings/SettingsForm.cs
+++ FLServerMaintainanceSettings/SettingsForm.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Windows.Forms;
+using Ini;
 
 namespace FLServerMaintainanceSettings
 {
     public partial class SettingsForm : Form
     {
         private static bool warnClose = true;
+        private static IniFile iniFile;
+        private static String SettingsFolder{get{return Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Sairento\\FLServerMaintainance\\";}}
+        private static String SettingsFile { get { return SettingsFolder+ "settings.ini"; } }
+
         public SettingsForm()
         {
             InitializeComponent();
+            if(!System.IO.Directory.Exists(SettingsFolder))
+                System.IO.Directory.CreateDirectory(SettingsFolder);
+
+            iniFile = new IniFile(SettingsFile);
         }
 
         private void BtnCancelClick(object sender, EventArgs e)
@@ -31,15 +40,17 @@ namespace FLServerMaintainanceSettings
         private void BtnSaveClick(object sender, EventArgs e)
         {
             //Save new settings
-            Properties.Settings.Default.FLFolder = txtFreelancerFolder.Text;
-            Properties.Settings.Default.AccountsFolder = txtAccountFolder.Text;
-            Properties.Settings.Default.BackupsFolder = txtBackupFolder.Text;
-            Properties.Settings.Default.ServerAddress = txtServerAddress.Text;
-            Properties.Settings.Default.ServerPassword = txtServerPassword.Text;
-            Properties.Settings.Default.ServerPort = Convert.ToInt32(numServerPort.Value);
-            Properties.Settings.Default.CloseProcesses = txtProcessesShutdown.Text;
-            Properties.Settings.Default.RestartProcesses = txtProcessesRestart.Text;
-            Properties.Settings.Default.Save();
+            iniFile.IniWriteValue("folders", "freelancer", txtFreelancerFolder.Text);
+            iniFile.IniWriteValue("folders", "accounts", txtAccountFolder.Text);
+            iniFile.IniWriteValue("folders", "backup", txtBackupFolder.Text);
+
+            iniFile.IniWriteValue("server", "address", txtServerAddress.Text);
+            iniFile.IniWriteValue("server", "port", numServerPort.Value.ToString());
+            iniFile.IniWriteValue("server", "password", txtServerPassword.Text);
+
+            iniFile.IniWriteValue("processes", "shutdown", txtProcessesShutdown.Text.Replace("\r\n",","));
+            iniFile.IniWriteValue("processes", "restart", txtProcessesRestart.Text.Replace("\r\n", ","));
+
             //close without warning
             warnClose = false;
             this.Close();
@@ -47,14 +58,17 @@ namespace FLServerMaintainanceSettings
 
         private void SettingsForm_Load(object sender, EventArgs e)
         {
-            txtFreelancerFolder.Text = Properties.Settings.Default.FLFolder;
-            txtAccountFolder.Text = Properties.Settings.Default.AccountsFolder;
-            txtBackupFolder.Text = Properties.Settings.Default.BackupsFolder;
-            txtServerAddress.Text = Properties.Settings.Default.ServerAddress;
-            txtServerPassword.Text = Properties.Settings.Default.ServerPassword;
-            numServerPort.Value = Properties.Settings.Default.ServerPort;
-            txtProcessesShutdown.Text = Properties.Settings.Default.CloseProcesses;
-            txtProcessesRestart.Text = Properties.Settings.Default.RestartProcesses;
+            //Read settings from file
+            txtFreelancerFolder.Text = iniFile.IniReadValue("folders", "freelancer");
+            txtAccountFolder.Text = iniFile.IniReadValue("folders", "accounts");
+            txtBackupFolder.Text = iniFile.IniReadValue("folders", "backup");
+
+            txtServerAddress.Text = iniFile.IniReadValue("server", "address");
+            numServerPort.Value = decimal.Parse(iniFile.IniReadValue("server", "port"));
+            txtServerPassword.Text = iniFile.IniReadValue("server", "password");
+
+            txtProcessesShutdown.Text = iniFile.IniReadValue("processes", "shutdown").Replace(",", "\r\n");
+            txtProcessesRestart.Text = iniFile.IniReadValue("processes", "restart").Replace(",", "\r\n");
         }
 
         private void BtnSetFLFolderClick(object sender, EventArgs e)
