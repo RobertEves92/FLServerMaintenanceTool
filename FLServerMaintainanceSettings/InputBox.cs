01702c0798067a9e69dd37dce5e057a9e44bab67
Robert Eves <roberteves92@gmail.com>
2015-10-07 16:54:23 +0100

    created input box form for adding and editing countdown items

---
+++ FLServerMaintainanceSettings/InputBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FLServerMaintainanceSettings
+{
+    public partial class InputBox : Form
+    {
+        public string ConsoleMessage { get { return txtConsole.Text; } }
+        public string UniverseMessage { get { return txtUniverse.Text; } }
+        public int WaitTime { get { return Convert.ToInt32(numWait.Value); } }
+
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+
+        public InputBox(string console, string universe, int wait)
+        {
+            InitializeComponent();
+            txtConsole.Text = console;
+            txtUniverse.Text = universe;
+            numWait.Value = wait;
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+    }
+}
