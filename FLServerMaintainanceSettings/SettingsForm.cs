febf8a59add6918213af9f94ab2fb4b9d5eb6eb1
Robert Eves <roberteves92@gmail.com>
2015-10-07 16:19:51 +0100

    check if file exists before loading

--- FLServerMaintainanceSettings/SettingsForm.cs
+++ FLServerMaintainanceSettings/SettingsForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using Ini;
 
@@ -51,6 +52,8 @@ namespace FLServerMaintainanceSettings
             iniFile.IniWriteValue("processes", "shutdown", txtProcessesShutdown.Text.Replace("\r\n",","));
             iniFile.IniWriteValue("processes", "restart", txtProcessesRestart.Text.Replace("\r\n", ","));
 
+            //add foreach loop here to save countdown messages
+
             //close without warning
             warnClose = false;
             this.Close();
@@ -58,17 +61,22 @@ namespace FLServerMaintainanceSettings
 
         private void SettingsForm_Load(object sender, EventArgs e)
         {
-            //Read settings from file
-            txtFreelancerFolder.Text = iniFile.IniReadValue("folders", "freelancer");
-            txtAccountFolder.Text = iniFile.IniReadValue("folders", "accounts");
-            txtBackupFolder.Text = iniFile.IniReadValue("folders", "backup");
+            if (File.Exists(SettingsFile))
+            {
+                //Read settings from file if the file exists
+                txtFreelancerFolder.Text = iniFile.IniReadValue("folders", "freelancer");
+                txtAccountFolder.Text = iniFile.IniReadValue("folders", "accounts");
+                txtBackupFolder.Text = iniFile.IniReadValue("folders", "backup");
 
-            txtServerAddress.Text = iniFile.IniReadValue("server", "address");
-            numServerPort.Value = decimal.Parse(iniFile.IniReadValue("server", "port"));
-            txtServerPassword.Text = iniFile.IniReadValue("server", "password");
+                txtServerAddress.Text = iniFile.IniReadValue("server", "address");
+                numServerPort.Value = decimal.Parse(iniFile.IniReadValue("server", "port"));
+                txtServerPassword.Text = iniFile.IniReadValue("server", "password");
 
-            txtProcessesShutdown.Text = iniFile.IniReadValue("processes", "shutdown").Replace(",", "\r\n");
-            txtProcessesRestart.Text = iniFile.IniReadValue("processes", "restart").Replace(",", "\r\n");
+                txtProcessesShutdown.Text = iniFile.IniReadValue("processes", "shutdown").Replace(",", "\r\n");
+                txtProcessesRestart.Text = iniFile.IniReadValue("processes", "restart").Replace(",", "\r\n");
+
+                //add foreach loop here to load countdown items
+            }
         }
 
         private void BtnSetFLFolderClick(object sender, EventArgs e)
