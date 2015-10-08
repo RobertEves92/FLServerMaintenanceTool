573f5c1f7bf7b195f10bf671ab60a1db11a35600
Robert Eves <roberteves92@gmail.com>
2015-10-08 10:31:21 +0100

    If no settings file is found, run the setting program

--- FLServerMaintenanceTool/Program.cs
+++ FLServerMaintenanceTool/Program.cs
@@ -8,13 +8,21 @@ namespace FLServerMaintenanceTool
         {
             Console.Title = "FL Server Maintainance";
 
-            Countdown.StartCountdown();
+            if (System.IO.File.Exists(Common.SettingsFile))
+            {
+                Countdown.StartCountdown();
 
-            Console.WriteLine("Auto Maintainance Started");
-            ProcessHelper.CloseProcesses();
-            Backup.RunBackup();
-            ProcessHelper.StartProcesses();
-            Console.WriteLine("Auto Maintainance Completed");
+                Console.WriteLine("Auto Maintainance Started");
+                ProcessHelper.CloseProcesses();
+                Backup.RunBackup();
+                ProcessHelper.StartProcesses();
+                Console.WriteLine("Auto Maintainance Completed");
+            }
+            else
+            {
+                Console.WriteLine("No settings file found.");
+                ProcessHelper.StartSettingsProcess();
+            }
             Console.ReadLine();
         }
     }
