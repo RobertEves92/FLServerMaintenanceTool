9f521e9974bc0f61cb682479a7455937085f6331
Robert Eves <roberteves92@gmail.com>
2015-10-21 01:22:32 +0100

    added method to get true or false status if players are online

--- FLServerMaintenanceTool/FLHookConnection.cs
+++ FLServerMaintenanceTool/FLHookConnection.cs
@@ -21,6 +21,7 @@ namespace FLServerMaintenanceTool
             var remoteEP = new IPEndPoint(ipAdd, int.Parse(Common.IniFile.IniReadValue("server", "port")));
             this.socket.Connect(remoteEP);
             this.SendCommand(string.Format("pass{0}", Common.IniFile.IniReadValue("server", "password")));
+            GetResponse();//Make sure the response buffer is clear after connection and validation
         }
 
         public void SendUniverseMessage(String message)
@@ -30,9 +31,10 @@ namespace FLServerMaintenanceTool
             #endif
         }
 
-        public void SendCommand(String command)
+        public string SendCommand(String command)
         {
             this.socket.Send(this.GetBytes(command));
+            return GetResponse();
         }
 
         public void Disconnect()
@@ -45,6 +47,26 @@ namespace FLServerMaintenanceTool
             //convert data string to byte array and add linebreaks
             byte[] byData = Encoding.ASCII.GetBytes(string.Format("{0}\r\n", data));
             return byData;
+        }
+
+        public bool PlayersOnline()
+        {
+            string response = SendCommand("getplayerids");
+            if (response == "OK\r\n")
+                return false;
+            else
+                return true;
+        }
+
+        private string GetResponse()
+        {
+            byte[] buffer = new byte[1024];
+            int iRx = this.socket.Receive(buffer);
+            char[] chars = new char[iRx];
+
+            System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+            d.GetChars(buffer,0,iRx,chars,0);
+            return new string(chars);
         }
     }
 }
